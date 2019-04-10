2090b010204fd240e0298510f30c0b3901cc0a2e
manasabalagoni1 <manasabalagoni1@gmail.com>
2019-04-10 14:36:24 +0530

    first commit

---
+++ Bitwise/AndOperator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitwise
+{
+    class AndOperator
+    {
+       int varA= 5;
+        int varB = 10;
+        int varC = 20;
+        public void result()
+        {
+
+
+            int result = ((varA & varB) | (varA & varC));
+
+            Console.WriteLine("{0} AND {1} result:{2}", varA, varB, result);
+        }
+        //comment
+        
+      
+    }
+}
